9e6491c19b7ee93a955faba65a546f37822d171e
Sean Wesenberg <wookets@gmail.com>
2012-05-08 14:23:42 -0500

    initial commit
    Signed-off-by: Sean Wesenberg <wookets@gmail.com>

---
+++ tojson.php
@@ -0,0 +1,47 @@
+<?php
+
+// Handles uploading and transform to json. 
+
+require_once './Classes/PHPExcel/IOFactory.php';
+
+if(!isset($_FILES['excelfile'])) {
+	echo json_encode((object) array('status' => 'error', 'code' => 'InvalidParameter', 'message' => 'The excelfile parameter not found'));
+	exit;
+}
+
+$inputFileName = $_FILES['excelfile']['name'];
+
+/**  Identify the type of $inputFileName  **/
+$inputFileType = PHPExcel_IOFactory::identify($inputFileName);
+/**  Create a new Reader of the type that has been identified  **/
+$objReader = PHPExcel_IOFactory::createReader($inputFileType);
+/**  Advise the Reader that we only want to load cell data  **/
+$objReader->setReadDataOnly(true);
+/**  Load $inputFileName to a PHPExcel Object  **/
+$objPHPExcel = $objReader->load($inputFileName);
+
+$excelArray = array();
+foreach ($objPHPExcel->getWorksheetIterator() as $worksheet) {
+	$worksheetArray = array();
+	//array_push($worksheetArray, $worksheet->getTitle());
+	
+	foreach ($worksheet->getRowIterator() as $row) {
+		$rowArray = array();
+		//array_push($excelArray, $row->getRowIndex());
+		
+		$cellIterator = $row->getCellIterator();
+		$cellIterator->setIterateOnlyExistingCells(false); // Loop all cells, even if it is not set
+		foreach ($cellIterator as $cell) {
+			//if (!is_null($cell)) {
+			//	array_push($excelArray, $cell->getCalculatedValue());
+			//}
+			array_push($rowArray, $cell->getCalculatedValue());
+		}
+		array_push($worksheetArray, $rowArray);
+	}
+	array_push($excelArray, $worksheetArray);
+}
+
+echo json_encode((object) array('status' => 'success', 'data' => $excelArray));
+
+?>
---
+++ tojson.test.php
@@ -0,0 +1,4 @@
+<form action="tojson.php" method="post" enctype="multipart/form-data"> 
+<input type="file"  name="excelfile" /> 
+<input type="submit" value="Upload" /> 
+</form> 
